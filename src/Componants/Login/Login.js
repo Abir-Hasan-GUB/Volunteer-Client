03bb7c3a3d90d5a1255036f71249b58355a25e42
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-10-04 23:55:15 +0600

    Add Firebage login System

--- src/Componants/Login/Login.js
+++ src/Componants/Login/Login.js
@@ -2,8 +2,34 @@ import React from 'react';
 import './Login.css';
 import logo from '../Images/logos/Logo.png';
 import google from '../Images/logos/google.png';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
 
 const Login = () => {
+    firebase.initializeApp(firebaseConfig);
+    const handleGoogleSignIn = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            console.log(user);
+            // ...
+          }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+          });
+
+    }
     return (
         <div className="container">
             <div className="login-logo">
@@ -13,7 +39,7 @@ const Login = () => {
             <div className="login-panel">
                 <div className="login-content">
                     <h4>Login With</h4>
-                    <div className="google">
+                    <div onClick={handleGoogleSignIn} className="google">
                         <img src= { google } alt="google icon"/>
                         <span className = "googleText">Continue with Google</span>
                     </div>
---
+++ src/Componants/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyClTIhILzq1zRYnzrrURdBiZgKxyvF0XmM",
+    authDomain: "volunteer-network-by-abir.firebaseapp.com",
+    databaseURL: "https://volunteer-network-by-abir.firebaseio.com",
+    projectId: "volunteer-network-by-abir",
+    storageBucket: "volunteer-network-by-abir.appspot.com",
+    messagingSenderId: "402568962504",
+    appId: "1:402568962504:web:9657b15cc4c2556dc2a0fd"
+  };
+  export default firebaseConfig;
