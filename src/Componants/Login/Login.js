05a571b373846e04457c09c92c847bd60f06bb6f
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-10-04 13:58:49 +0600

    Login Part Complete

--- src/Componants/Login/Login.js
+++ src/Componants/Login/Login.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Login.css';
+import logo from '../Images/logos/Logo.png';
+import google from '../Images/logos/google.png';
 
 const Login = () => {
     return (
-        <div>
-            <h1>This is Login</h1>
+        <div className="container">
+            <div className="login-logo">
+            <img src= {logo} alt=""/>
+            </div>
+            
+            <div className="login-panel">
+                <div className="login-content">
+                    <h4>Login With</h4>
+                    <div className="google">
+                        <img src= { google } alt="google icon"/>
+                        <span className = "googleText">Continue with Google</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
